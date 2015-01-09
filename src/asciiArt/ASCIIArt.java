81014881a5df49b0d25f421046016b87afa2aecc
mattpascoe135 <matt.pascoe@uqconnect.edu.au>
2015-01-09 21:40:40 +1000

    Final Commit
    Updated to include scaling on image, properlly read the image.

--- src/asciiArt/ASCIIArt.java
+++ src/asciiArt/ASCIIArt.java
@@ -2,53 +2,71 @@ package asciiArt;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.imageio.ImageIO;
 
 public class ASCIIArt {
+	/**
+	 * Takes a string giving the location of an image (GIF, PNG, or JPEG image 
+	 * file), and converts it into ASCII text. The result is outputted into 
+	 * a text file.
+	 * 
+	 * @param args
+	 * 		filename, the filename of the image to convert into ASCII
+	 * 		scale, the new scale of the image in perentage. 
+	 * 			Note: 100=current size, 50=halfsize, 200=double size
+	 */
 	public static void main(String[] args){
 		String fileName = args[0];
 		
+		int scale = 0;
 		double pixelVal = 0;
 		
-		BufferedWriter writer = null;
+		PrintWriter writer = null;
 		BufferedImage img = null;
 		try {
+			scale = Integer.parseInt(args[1]);
 		    img = ImageIO.read(new File(fileName));
-		    File logFile = new File("output.txt");
-		    writer = new BufferedWriter(new FileWriter(logFile));
+		    writer = new PrintWriter("output.txt");
 		} catch (IOException e) {
 			System.out.println("Couldn't find the image");
 		}
+
+		//Resize the image
+		int newHeight = img.getHeight()*scale/100;
+		int newWidth = img.getWidth()*scale/100;
 		
-		int width = img.getWidth();
-		int height = img.getHeight();
+		BufferedImage newImg = new BufferedImage(newWidth, newHeight, 
+				img.getType());
+		newImg.getGraphics().drawImage(img, 0, 0, newWidth, newHeight, null);
 		
-		System.out.println("width: " + width + ", height: " + height);
-		
-		for(int i=0; i<height; i++){
+		for(int i=0; i<newImg.getHeight(); i++){
 			String line = "";
-			int num = 0;
-			for(int j=0; j<width; j++){
-				Color pixel = new Color(img.getRGB(j, i));
-				pixelVal = ((pixel.getRed()*0.3) + (pixel.getBlue()*0.59) + pixel.getGreen()*0.11);
+			for(int j=0; j<newImg.getWidth(); j++){
+				Color pixel = new Color(newImg.getRGB(j, i));
+				//Convert color into single rgb value
+				pixelVal = ((pixel.getRed()*0.3) + (pixel.getBlue()*0.59) 
+						+ pixel.getGreen()*0.11);
 				line += addChar(pixelVal);
-				num++;
 			}
-			try {
-				writer.write(line);
-				writer.newLine();
-			} catch (IOException e) {
-				System.out.println("ERROR occured");
-			}
+			writer.println(line);		
 		}
+		writer.close();
 		
 	}
 	
+	/**
+	 * Takes an RBG value for a pixel and returns an appropriate 
+	 * ASCII character
+	 * 
+	 * @param pixelVal
+	 * 		A single RGB value for a given pixel
+	 * @return
+	 * 		A corresponding ASCII symbol for the pixel
+	 */
 	public static String addChar(double pixelVal){
 		String str = " ";
         if (pixelVal >= 240) {
